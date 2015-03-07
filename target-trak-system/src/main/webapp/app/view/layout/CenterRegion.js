5df8c0c9d9e4ed16e75485878ca2bc04fafd9058
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-07 18:55:20 -0500

    New center region panel for page navigation

---
+++ target-trak-system/src/main/webapp/app/view/layout/CenterRegion.js
@@ -0,0 +1,12 @@
+Ext.define('TGT.view.layout.CenterRegion', {
+	extend : 'Ext.panel.Panel',
+	alias : 'widget.layout.centerregion',
+	layout: 'card',
+    items: [
+        {
+        	xtype: 'panel',
+        	title: 'Hi',
+        	activeItem: 0
+        }
+    ]
+});
