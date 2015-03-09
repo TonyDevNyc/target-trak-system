771037859a71acff3bf73bac269be777327c7621
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-09 15:33:18 -0400

    Remove validations package from security

--- target-trak-system/src/main/java/com/target/trak/system/security/validations/TargetTrakValidator.java
+++
@@ -1,8 +0,0 @@
-package com.target.trak.system.security.validations;
-
-import java.util.List;
-
-public interface TargetTrakValidator<T> {
-
-	public List<SecurityValidationError> validate(T object) throws IllegalArgumentException;
-}
