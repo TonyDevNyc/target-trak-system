cf49934eb121f4a4ff2ae8f10cc249fdcc5129a1
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-15 22:04:01 -0400

    initial revision reference data ui model

---
+++ target-trak-system/src/main/java/com/target/trak/system/web/views/ui/refdata/ReferenceDataModel.java
@@ -0,0 +1,78 @@
+package com.target.trak.system.web.views.ui.refdata;
+
+public class ReferenceDataModel {
+
+	private Long id;
+	private String type;
+	private String label;
+	private String value;
+	private String createdBy;
+	private String createdDateTime;
+	private String lastUpdatedBy;
+	private String lastUpdatedDateTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedDateTime() {
+		return createdDateTime;
+	}
+
+	public void setCreatedDateTime(String createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+
+	public String getLastUpdatedBy() {
+		return lastUpdatedBy;
+	}
+
+	public void setLastUpdatedBy(String lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
+	}
+
+	public String getLastUpdatedDateTime() {
+		return lastUpdatedDateTime;
+	}
+
+	public void setLastUpdatedDateTime(String lastUpdatedDateTime) {
+		this.lastUpdatedDateTime = lastUpdatedDateTime;
+	}
+
+}
