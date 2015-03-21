2ced1bb434f06e16cf013b5be4fb1778174d7be9
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-21 18:33:51 -0400

    change api

--- target-trak-system/src/main/java/com/target/trak/system/security/audit/service/impl/AuditServiceImpl.java
+++ target-trak-system/src/main/java/com/target/trak/system/security/audit/service/impl/AuditServiceImpl.java
@@ -1,13 +1,18 @@
 package com.target.trak.system.security.audit.service.impl;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.target.trak.system.security.audit.AuditableEvent;
 import com.target.trak.system.security.audit.dao.AuditEventDao;
 import com.target.trak.system.security.audit.domain.AuditEvent;
 import com.target.trak.system.security.audit.service.AuditService;
+import com.target.trak.system.security.context.UserContext;
 
 @Transactional(propagation=Propagation.REQUIRES_NEW)
 @Service("auditEventService")
@@ -16,9 +21,39 @@ public class AuditServiceImpl implements AuditService {
 	@Autowired
 	private AuditEventDao auditEventDao;
 	
+	@Autowired
+	private UserContext userContext;
+	
 	@Override
-	public AuditEvent createAuditEvent(final AuditEvent auditEvent) {
-		return auditEventDao.insertAuditEvent(auditEvent);
+	public void createAuditEvent(final AuditableEvent auditableEvent, final Throwable exception) {
+		AuditEvent auditEvent = buildAuditEvent(auditableEvent, exception);
+		auditEventDao.insertAuditEvent(auditEvent);
 	}
+	
+	private AuditEvent buildAuditEvent(final AuditableEvent auditableEvent, final Throwable exception) {
+		AuditEvent auditEvent = new AuditEvent();
+		auditEvent.setAuditEventCode(auditableEvent.auditableEventCode().toString());
+		auditEvent.setAuditEventDescription(auditableEvent.auditableEventCode().description());
+		auditEvent.setUsername(getCurrentUser());
+		Calendar currentTime = Calendar.getInstance();
+		auditEvent.setTimestamp(new Timestamp(currentTime.getTimeInMillis()));
+
+		boolean isSuccessful = exception == null;
+		auditEvent.setSuccess(isSuccessful);
+		if (!isSuccessful) {
+			auditEvent.setErrorMessage(exception.getMessage());
+		}
+		return auditEvent;
+	}
+	
+	private String getCurrentUser() {
+		if (userContext == null || userContext.getCurrentUser() == null) {
+			return "Anonymous User";
+		} else {
+			return userContext.getCurrentUser().getUsername();
+		}
+	}
+
+
 
 }
