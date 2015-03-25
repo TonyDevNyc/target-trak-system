b96d4bfda04be6b71d71b2c8c42628f938921cff
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-24 20:25:17 -0400

    change registration response and request dto to extend TargetTrakApiRequest and TargetTrakApiResponse

--- target-trak-system/src/main/java/com/target/trak/system/security/dto/registration/RegistrationApiRequest.java
+++ target-trak-system/src/main/java/com/target/trak/system/security/dto/registration/RegistrationApiRequest.java
@@ -1,8 +1,9 @@
 package com.target.trak.system.security.dto.registration;
 
 import com.target.trak.system.security.dto.UserDto;
+import com.target.trak.system.service.dto.TargetTrakApiRequest;
 
-public class RegistrationApiRequest {
+public class RegistrationApiRequest extends TargetTrakApiRequest {
 
 	private UserDto userRegistration;
 
--- target-trak-system/src/main/java/com/target/trak/system/security/dto/registration/RegistrationApiResponse.java
+++ target-trak-system/src/main/java/com/target/trak/system/security/dto/registration/RegistrationApiResponse.java
@@ -1,37 +1,8 @@
 package com.target.trak.system.security.dto.registration;
 
-import java.util.List;
+import com.target.trak.system.service.dto.TargetTrakApiResponse;
 
-import com.target.trak.system.validations.TargetTrakValidationError;
+public class RegistrationApiResponse extends TargetTrakApiResponse {
 
-public class RegistrationApiResponse {
-
-	private boolean success;
-	private List<TargetTrakValidationError> errors;
-	private String errorMessage;
-
-	public boolean isSuccess() {
-		return success;
-	}
-
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
-
-	public List<TargetTrakValidationError> getErrors() {
-		return errors;
-	}
-
-	public void setErrors(List<TargetTrakValidationError> errors) {
-		this.errors = errors;
-	}
-
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
 
 }
