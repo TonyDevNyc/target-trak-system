25659a69d359a5ce95ef4567dff469e24d63e46e
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-26 22:26:55 -0400

    refactor service apis to no longer return TargetTrakApiResponse subclasses for errors, TargetTrakException are thrown

--- target-trak-system/src/main/java/com/target/trak/system/service/impl/ReferenceDataServiceImpl.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/impl/ReferenceDataServiceImpl.java
@@ -14,9 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.target.trak.system.dao.ReferenceDataDao;
 import com.target.trak.system.domain.ReferenceDataDomain;
 import com.target.trak.system.domain.ReferenceDataSearchCriteria;
-import com.target.trak.system.security.audit.AuditableEvent;
-import com.target.trak.system.security.audit.TargetTrakAuditEventCode;
 import com.target.trak.system.service.ReferenceDataService;
+import com.target.trak.system.service.dto.TargetTrakApiResponse;
+import com.target.trak.system.service.dto.TargetTrakErrorTypeEnum;
 import com.target.trak.system.service.dto.TargetTrakRequestTypeEnum;
 import com.target.trak.system.service.dto.referencedata.ReferenceDataApiRequest;
 import com.target.trak.system.service.dto.referencedata.ReferenceDataApiResponse;
@@ -26,7 +26,7 @@ import com.target.trak.system.validations.TargetTrakValidationError;
 import com.target.trak.system.validations.TargetTrakValidationException;
 import com.target.trak.system.validations.impl.ReferenceDataValidatorImpl;
 
-@Transactional(value = "dwTransactionManager", propagation = Propagation.REQUIRED, rollbackFor = TargetTrakException.class)
+@Transactional(value = "dwTransactionManager", propagation = Propagation.REQUIRED)
 @Service("referenceDataService")
 public class ReferenceDataServiceImpl implements ReferenceDataService {
 
@@ -42,51 +42,52 @@ public class ReferenceDataServiceImpl implements ReferenceDataService {
 	@Autowired
 	private ReferenceDataValidatorImpl validator;
 
-	@AuditableEvent(auditableEventCode=TargetTrakAuditEventCode.CREATE_REFERENCE_DATA)
 	@Override
 	public ReferenceDataApiResponse createReferenceData(final ReferenceDataApiRequest request) throws TargetTrakException {
 		ReferenceDataApiResponse response = new ReferenceDataApiResponse();
 		request.setRequestType(TargetTrakRequestTypeEnum.CREATE);
 		List<TargetTrakValidationError> validationErrors = validateRequest(request);
-		try {
-			if (validationErrors.isEmpty()) {
-				ReferenceDataDomain domain = referenceDataDao.insertReferenceData(conversionService.convert(request.getReferenceDataDto(), ReferenceDataDomain.class));
-				response.setReferenceData(conversionService.convert(domain, ReferenceDataDto.class));
-				response.setSuccess(Boolean.TRUE);
 
-			} else {
-				response.setMessage("A validation error has occurred. Please fix the errors below");
-				response.setSuccess(Boolean.FALSE);
-				response.setErrors(validationErrors);
-			}
-		} catch (Exception e) {
+		if (!validationErrors.isEmpty()) {
+			TargetTrakException exception = generateServiceException(validationErrors, TargetTrakErrorTypeEnum.VALIDATION, "A validation error has occurred. Please fix the errors below");
+			throw exception;
+		}
+
+		try {
+			ReferenceDataDomain domain = referenceDataDao.insertReferenceData(conversionService.convert(request.getReferenceDataDto(), ReferenceDataDomain.class));
+			response.setReferenceData(conversionService.convert(domain, ReferenceDataDto.class));
+			response.setSuccess(Boolean.TRUE);
+		} catch (Throwable e) {
 			logger.error(e.getMessage(), e);
-			response.setSuccess(Boolean.FALSE);
-			response.setMessage("An error has occurred trying to create Reference Data. <br /> If the error still occurs, contact your administrator");
+			TargetTrakException exception = generateServiceException(validationErrors, TargetTrakErrorTypeEnum.ERROR, "An error has occurred trying to create Reference Data. <br /> If the error still occurs, contact your administrator");
+			throw exception;
 		}
 		return response;
 	}
 
-	@AuditableEvent(auditableEventCode=TargetTrakAuditEventCode.READ_REFERENCE_DATA)
 	@Transactional(propagation = Propagation.NEVER)
 	@Override
 	public ReferenceDataApiResponse getReferenceDataByCriteria(final ReferenceDataApiRequest request) throws TargetTrakException {
 		ReferenceDataApiResponse response = new ReferenceDataApiResponse();
 		ReferenceDataSearchCriteria criteria = conversionService.convert(request.getSearchCriteria(), ReferenceDataSearchCriteria.class);
-
-		int totalSize = referenceDataDao.selectReferenceDataBySearchCriteriaCount(criteria);
-		if (totalSize > 0) {
-			List<ReferenceDataDomain> data = referenceDataDao.selectPaginatedReferenceDataBySearchCriteria(criteria);
-			List<ReferenceDataDto> dtos = convertListOfDomains(data);
-			response.setReferenceDataList(dtos);
+		try {
+			int totalSize = referenceDataDao.selectReferenceDataBySearchCriteriaCount(criteria);
+			if (totalSize > 0) {
+				List<ReferenceDataDomain> data = referenceDataDao.selectPaginatedReferenceDataBySearchCriteria(criteria);
+				List<ReferenceDataDto> dtos = convertListOfDomains(data);
+				response.setReferenceDataList(dtos);
+			}
+			response.setTotalSize(totalSize);
+			response.setSuccess(Boolean.TRUE);
+		} catch(Throwable e) {
+			logger.error(e.getMessage(), e);
+			TargetTrakException exception = generateServiceException(null, TargetTrakErrorTypeEnum.ERROR, "An error has occurred trying to search Reference Data. <br /> If the error still occurs, contact your administrator");
+			throw exception;
 		}
-		response.setTotalSize(totalSize);
-		response.setSuccess(Boolean.TRUE);
 
 		return response;
 	}
 
-	@AuditableEvent(auditableEventCode=TargetTrakAuditEventCode.READ_REFERENCE_DATA)
 	@Transactional(propagation = Propagation.NEVER)
 	@Override
 	public ReferenceDataApiResponse getReferenceDataTypes() {
@@ -98,56 +99,64 @@ public class ReferenceDataServiceImpl implements ReferenceDataService {
 		return response;
 	}
 
-	@AuditableEvent(auditableEventCode=TargetTrakAuditEventCode.UPDATE_REFERENCE_DATA)
 	@Override
 	public ReferenceDataApiResponse updateReferenceData(final ReferenceDataApiRequest request) throws TargetTrakException {
 		ReferenceDataApiResponse response = new ReferenceDataApiResponse();
 		request.setRequestType(TargetTrakRequestTypeEnum.UPDATE);
 		List<TargetTrakValidationError> validationErrors = validateRequest(request);
 
+		if (!validationErrors.isEmpty()) {
+			TargetTrakException exception = generateServiceException(validationErrors, TargetTrakErrorTypeEnum.VALIDATION, "A validation error has occurred. Please fix the errors below");
+			throw exception;
+		}
+
 		try {
-			if (validationErrors.isEmpty()) {
-				ReferenceDataDto requestDto = request.getReferenceDataDto();
-				ReferenceDataDomain domain = referenceDataDao.updateReferenceData(conversionService.convert(requestDto, ReferenceDataDomain.class));
-				ReferenceDataDto dto = conversionService.convert(domain, ReferenceDataDto.class);
-				response.setReferenceData(dto);
-				response.setSuccess(Boolean.TRUE);
-			} else {
-				response.setMessage("A validation error has occurred. Please fix the errors below");
-				response.setSuccess(Boolean.FALSE);
-				response.setErrors(validationErrors);
-			}
+			ReferenceDataDto requestDto = request.getReferenceDataDto();
+			ReferenceDataDomain domain = referenceDataDao.updateReferenceData(conversionService.convert(requestDto, ReferenceDataDomain.class));
+			ReferenceDataDto dto = conversionService.convert(domain, ReferenceDataDto.class);
+			response.setReferenceData(dto);
+			response.setSuccess(Boolean.TRUE);
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
-			response.setSuccess(Boolean.FALSE);
-			response.setMessage("An error has occurred trying to update Reference Data. <br /> If the error still occurs, contact your administrator");
+			TargetTrakException exception = generateServiceException(validationErrors, TargetTrakErrorTypeEnum.ERROR, "An error has occurred trying to update Reference Data. <br /> If the error still occurs, contact your administrator");
+			throw exception;
 		}
 		return response;
 	}
 
-	@AuditableEvent(auditableEventCode=TargetTrakAuditEventCode.DELETE_REFERENCE_DATA)
 	@Override
 	public ReferenceDataApiResponse deleteReferenceData(final ReferenceDataApiRequest request) throws TargetTrakException {
 		ReferenceDataApiResponse response = new ReferenceDataApiResponse();
 		request.setRequestType(TargetTrakRequestTypeEnum.DELETE);
 		List<TargetTrakValidationError> validationErrors = validateRequest(request);
 
+		if (!validationErrors.isEmpty()) {
+			TargetTrakException exception = generateServiceException(validationErrors, TargetTrakErrorTypeEnum.VALIDATION, "A validation error has occurred. Please fix the errors below");
+			throw exception;
+		}
+
 		try {
-			if (validationErrors.isEmpty()) {
-				ReferenceDataDto requestDto = request.getReferenceDataDto();
-				referenceDataDao.deleteReferenceData(conversionService.convert(requestDto, ReferenceDataDomain.class));
-				response.setSuccess(Boolean.TRUE);
-			} else {
-				response.setMessage("A validation error has occurred. Please fix the errors below");
-				response.setSuccess(Boolean.FALSE);
-				response.setErrors(validationErrors);
-			}
+			ReferenceDataDto requestDto = request.getReferenceDataDto();
+			referenceDataDao.deleteReferenceData(conversionService.convert(requestDto, ReferenceDataDomain.class));
+			response.setSuccess(Boolean.TRUE);
+
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
-			response.setSuccess(Boolean.FALSE);
-			response.setMessage("An error has occurred trying to delete Reference Data. <br /> If the error still occurs, contact your administrator");
+			TargetTrakException exception = generateServiceException(validationErrors, TargetTrakErrorTypeEnum.ERROR, "An error has occurred trying to delete Reference Data. <br /> If the error still occurs, contact your administrator");
+			throw exception;
 		}
 		return response;
+	}
+
+	private TargetTrakException generateServiceException(final List<TargetTrakValidationError> validationErrors, final TargetTrakErrorTypeEnum errorType, final String message) {
+		TargetTrakException exception = new TargetTrakException(message);
+		TargetTrakApiResponse response = new TargetTrakApiResponse();
+		response.setSuccess(Boolean.FALSE);
+		response.setErrorType(errorType);
+		response.setErrors(validationErrors);
+		response.setMessage(message);
+		exception.setResponse(response);
+		return exception;
 	}
 
 	private List<TargetTrakValidationError> validateRequest(final ReferenceDataApiRequest request) throws TargetTrakException {
