88b5446413c9a28dc3c61a1a46640bda2c5e5588
TonyDevNyc <tonydevgroupny@gmail.com>
2015-03-26 22:25:18 -0400

    added TargetTrakApiResponse to TargetTrakException to propogate information to client

--- target-trak-system/src/main/java/com/target/trak/system/service/exception/TargetTrakException.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/exception/TargetTrakException.java
@@ -1,14 +1,27 @@
 package com.target.trak.system.service.exception;
 
-public class TargetTrakException extends Exception {
+import com.target.trak.system.service.dto.TargetTrakApiResponse;
+
+public class TargetTrakException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	private TargetTrakApiResponse response;
+	
 	public TargetTrakException() {
 		super();
 	}
-	
+
 	public TargetTrakException(String msg) {
 		super(msg);
 	}
+
+	public TargetTrakApiResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(TargetTrakApiResponse response) {
+		this.response = response;
+	}
+
 }
