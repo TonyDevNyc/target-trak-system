36a47dcf831f1343cc87ab0bf57a80e1b861ebc7
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-03 14:31:09 -0400

    additional company service apis

---
+++ target-trak-system/src/main/java/com/target/trak/system/service/impl/company/UpdateCompanyServiceImpl.java
@@ -0,0 +1,25 @@
+package com.target.trak.system.service.impl.company;
+
+import java.util.List;
+
+import com.target.trak.system.service.TargetTrakService;
+import com.target.trak.system.service.dto.company.CompanyApiRequest;
+import com.target.trak.system.service.dto.company.CompanyApiResponse;
+import com.target.trak.system.service.exception.TargetTrakException;
+import com.target.trak.system.validations.TargetTrakValidationError;
+
+public class UpdateCompanyServiceImpl implements TargetTrakService<CompanyApiRequest, CompanyApiResponse> {
+
+	@Override
+	public CompanyApiResponse executeRequest(CompanyApiRequest request) throws TargetTrakException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TargetTrakValidationError> validateRequest(CompanyApiRequest request) throws TargetTrakException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
