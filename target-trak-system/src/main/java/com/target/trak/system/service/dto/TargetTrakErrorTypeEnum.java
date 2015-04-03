32c3bc9771bf583d295ea3989f9964878e2cf14a
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-03 14:19:12 -0400

    remove enums from dto package and moved to dto.common package

--- target-trak-system/src/main/java/com/target/trak/system/service/dto/TargetTrakErrorTypeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.target.trak.system.service.dto;
-
-public enum TargetTrakErrorTypeEnum {
-
-	ERROR, WARNING, VALIDATION;
-
-}
--- target-trak-system/src/main/java/com/target/trak/system/service/dto/TargetTrakRequestTypeEnum.java
+++
@@ -1,6 +0,0 @@
-package com.target.trak.system.service.dto;
-
-public enum TargetTrakRequestTypeEnum {
-
-	CREATE, READ_BY_ID, READ_ALL, READ_PAGING, UPDATE, DELETE
-}
