733098f426026d625bfdb284e531deef62a33c73
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-03 14:18:18 -0400

    initial revision -refactoring service layer

---
+++ target-trak-system/src/main/java/com/target/trak/system/service/AbstractTargetTrakService.java
@@ -0,0 +1,22 @@
+package com.target.trak.system.service;
+
+import java.util.List;
+
+import com.target.trak.system.service.dto.TargetTrakApiResponse;
+import com.target.trak.system.service.dto.common.TargetTrakErrorTypeEnum;
+import com.target.trak.system.service.exception.TargetTrakException;
+import com.target.trak.system.validations.TargetTrakValidationError;
+
+public class AbstractTargetTrakService {
+
+	public final TargetTrakException generateServiceException(final List<TargetTrakValidationError> validationErrors, final TargetTrakErrorTypeEnum errorType, final String message) {
+		TargetTrakException exception = new TargetTrakException(message);
+		TargetTrakApiResponse response = new TargetTrakApiResponse();
+		response.setSuccess(Boolean.FALSE);
+		response.setErrorType(errorType);
+		response.setErrors(validationErrors);
+		response.setMessage(message);
+		exception.setResponse(response);
+		return exception;
+	}
+}
---
+++ target-trak-system/src/main/java/com/target/trak/system/service/TargetTrakService.java
@@ -0,0 +1,15 @@
+package com.target.trak.system.service;
+
+import java.util.List;
+
+import com.target.trak.system.service.dto.common.TargetTrakApiRequest;
+import com.target.trak.system.service.dto.common.TargetTrakApiResponse;
+import com.target.trak.system.service.exception.TargetTrakException;
+import com.target.trak.system.validations.TargetTrakValidationError;
+
+public interface TargetTrakService<T extends TargetTrakApiRequest, S extends TargetTrakApiResponse> {
+
+	public S executeRequest(final T request) throws TargetTrakException;
+	
+	public List<TargetTrakValidationError> validateRequest(final T request) throws TargetTrakException;
+}
