8bed7e9dc52056f7b45b24904f32a3f0cb6166c9
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-04 19:31:48 -0400

    refactor base service class to be abstract and takes a TargetTrakApiResponse

--- target-trak-system/src/main/java/com/target/trak/system/service/BaseTargetTrakService.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/BaseTargetTrakService.java
@@ -2,16 +2,19 @@ package com.target.trak.system.service;
 
 import java.util.List;
 
-import com.target.trak.system.service.dto.TargetTrakApiResponse;
+import com.target.trak.system.service.dto.common.TargetTrakApiResponse;
 import com.target.trak.system.service.dto.common.TargetTrakErrorTypeEnum;
 import com.target.trak.system.service.exception.TargetTrakException;
 import com.target.trak.system.validations.TargetTrakValidationError;
 
-public class BaseTargetTrakService {
+public abstract class BaseTargetTrakService {
 
-	public final TargetTrakException generateServiceException(final List<TargetTrakValidationError> validationErrors, final TargetTrakErrorTypeEnum errorType, final String message) {
+	public final TargetTrakException generateServiceException(
+			final TargetTrakApiResponse response,
+			final List<TargetTrakValidationError> validationErrors, 
+			final TargetTrakErrorTypeEnum errorType, 
+			final String message) {
 		TargetTrakException exception = new TargetTrakException(message);
-		TargetTrakApiResponse response = new TargetTrakApiResponse();
 		response.setSuccess(Boolean.FALSE);
 		response.setErrorType(errorType);
 		response.setErrors(validationErrors);
