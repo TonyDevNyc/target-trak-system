a350d7d3758f055658110a7696481855e6344e52
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-13 16:49:15 -0400

    added methods to interface for paging criteria

--- target-trak-system/src/main/java/com/target/trak/system/service/dto/common/TargetTrakApiRequest.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/dto/common/TargetTrakApiRequest.java
@@ -3,7 +3,10 @@ package com.target.trak.system.service.dto.common;
 public interface TargetTrakApiRequest {
 
 	public TargetTrakRequestTypeEnum getRequestType();
-	
+
 	public void setRequestType(final TargetTrakRequestTypeEnum requestType);
-	
+
+	public TargetTrakPagingCriteria getPagingCriteria();
+
+	public void setPagingCriteria(TargetTrakPagingCriteria criteria);
 }
