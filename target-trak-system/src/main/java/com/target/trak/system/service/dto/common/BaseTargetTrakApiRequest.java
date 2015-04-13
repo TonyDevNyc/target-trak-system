5aabe84969a8e7644b7cde305a4c7a1777cbd5a8
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-13 16:48:20 -0400

    added PagingCriteria to base dto

--- target-trak-system/src/main/java/com/target/trak/system/service/dto/common/BaseTargetTrakApiRequest.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/dto/common/BaseTargetTrakApiRequest.java
@@ -3,6 +3,17 @@ package com.target.trak.system.service.dto.common;
 public abstract class BaseTargetTrakApiRequest implements TargetTrakApiRequest {
 
 	private TargetTrakRequestTypeEnum requestType;
+	private TargetTrakPagingCriteria pagingCriteria;
+
+	@Override
+	public TargetTrakPagingCriteria getPagingCriteria() {
+		return pagingCriteria;
+	}
+
+	@Override
+	public void setPagingCriteria(TargetTrakPagingCriteria criteria) {
+		this.pagingCriteria = criteria;
+	}
 
 	@Override
 	public TargetTrakRequestTypeEnum getRequestType() {
