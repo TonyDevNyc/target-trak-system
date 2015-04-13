d61beb2f3a0393198790cc82397f2ed108e2ef39
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-13 16:51:30 -0400

    class now extends paging criteria

--- target-trak-system/src/main/java/com/target/trak/system/service/dto/referencedata/ReferenceDataSearchCriteriaDto.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/dto/referencedata/ReferenceDataSearchCriteriaDto.java
@@ -1,14 +1,11 @@
 package com.target.trak.system.service.dto.referencedata;
 
-public class ReferenceDataSearchCriteriaDto {
+import com.target.trak.system.service.dto.common.TargetTrakPagingCriteria;
+
+public class ReferenceDataSearchCriteriaDto extends TargetTrakPagingCriteria {
 
 	private String referenceDataType;
 	private String referenceDataLabel;
-	private int start;
-	private int end;
-	private int page;
-	private String sortField;
-	private String sortDirection;
 
 	public String getReferenceDataType() {
 		return referenceDataType;
@@ -26,43 +23,4 @@ public class ReferenceDataSearchCriteriaDto {
 		this.referenceDataLabel = referenceDataLabel;
 	}
 
-	public int getStart() {
-		return start;
-	}
-
-	public void setStart(int start) {
-		this.start = start;
-	}
-
-	public int getEnd() {
-		return end;
-	}
-
-	public void setEnd(int end) {
-		this.end = end;
-	}
-
-	public int getPage() {
-		return page;
-	}
-
-	public void setPage(int page) {
-		this.page = page;
-	}
-
-	public String getSortField() {
-		return sortField;
-	}
-
-	public void setSortField(String sortField) {
-		this.sortField = sortField;
-	}
-
-	public String getSortDirection() {
-		return sortDirection;
-	}
-
-	public void setSortDirection(String sortDirection) {
-		this.sortDirection = sortDirection;
-	}
 }
