b9aca2c52c82dbe983b3eaf069fbf914066f25e0
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-13 16:50:56 -0400

    dto now extends super class

--- target-trak-system/src/main/java/com/target/trak/system/service/dto/company/CompanySearchCriteriaDto.java
+++ target-trak-system/src/main/java/com/target/trak/system/service/dto/company/CompanySearchCriteriaDto.java
@@ -1,15 +1,12 @@
 package com.target.trak.system.service.dto.company;
 
-public class CompanySearchCriteriaDto {
+import com.target.trak.system.service.dto.common.TargetTrakPagingCriteria;
+
+public class CompanySearchCriteriaDto extends TargetTrakPagingCriteria {
 
 	private String name;
 	private String state;
 	private String country;
-	private int start;
-	private int end;
-	private int page;
-	private String sortField;
-	private String sortDirection;
 
 	public String getName() {
 		return name;
@@ -33,46 +30,6 @@ public class CompanySearchCriteriaDto {
 
 	public void setCountry(String country) {
 		this.country = country;
-	}
-
-	public int getStart() {
-		return start;
-	}
-
-	public void setStart(int start) {
-		this.start = start;
-	}
-
-	public int getEnd() {
-		return end;
-	}
-
-	public void setEnd(int end) {
-		this.end = end;
-	}
-
-	public int getPage() {
-		return page;
-	}
-
-	public void setPage(int page) {
-		this.page = page;
-	}
-
-	public String getSortField() {
-		return sortField;
-	}
-
-	public void setSortField(String sortField) {
-		this.sortField = sortField;
-	}
-
-	public String getSortDirection() {
-		return sortDirection;
-	}
-
-	public void setSortDirection(String sortDirection) {
-		this.sortDirection = sortDirection;
 	}
 
 }
