8508f103a688d28da1dee2947c12ad9c8078280f
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-16 17:42:49 -0400

    initialize base controller class

--- target-trak-system/src/main/webapp/app/controller/BaseController.js
+++ target-trak-system/src/main/webapp/app/controller/BaseController.js
@@ -1,3 +1,7 @@
 Ext.define('TGT.controller.BaseController', {
-	extend: 'Ext.app.Controller'
+	extend: 'Ext.app.Controller',
+	
+	 init: function() {
+		 console.log('inside base controller');
+	 }
 });
