235019241ea5ad7aa7c7432ac853206c6fb87804
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-20 20:58:13 -0400

    reformatting

--- target-trak-system/src/main/java/com/target/trak/system/security/audit/service/AuditService.java
+++ target-trak-system/src/main/java/com/target/trak/system/security/audit/service/AuditService.java
@@ -3,7 +3,6 @@ package com.target.trak.system.security.audit.service;
 import com.target.trak.system.security.audit.AuditableEvent;
 import com.target.trak.system.service.dto.common.TargetTrakApiResponse;
 
-
 public interface AuditService {
 
 	public void createAuditEvent(final AuditableEvent auditableEvent, final TargetTrakApiResponse response);
