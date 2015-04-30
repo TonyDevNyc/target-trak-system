58cddf0dfe520df3afb12c2e602f9b87f686b09e
TonyDevNyc <tonydevgroupny@gmail.com>
2015-04-30 19:11:10 -0400

    add api error building

--- target-trak-system/src/main/java/com/target/trak/system/web/exception/TargetTrakWebExceptionHandler.java
+++ target-trak-system/src/main/java/com/target/trak/system/web/exception/TargetTrakWebExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 import com.target.trak.system.service.dto.common.TargetTrakApiResponse;
 import com.target.trak.system.service.dto.common.TargetTrakErrorTypeEnum;
 import com.target.trak.system.service.exception.TargetTrakException;
+import com.target.trak.system.validations.TargetTrakValidationError;
 import com.target.trak.system.web.views.ui.common.UIErrorBuilder;
 import com.target.trak.system.web.views.ui.common.UIValidationError;
 
@@ -24,32 +25,32 @@ public class TargetTrakWebExceptionHandler {
 	private static final String JSON_MESSAGE = "message";
 
 	private UIErrorBuilder errorBuilder;
-	
+
 	private Logger logger = Logger.getLogger(getClass());
-	
+
 	@ExceptionHandler({ TargetTrakException.class })
 	public @ResponseBody
 	Map<String, Object> handleServiceException(RuntimeException e, WebRequest request) {
 		TargetTrakException exception = (TargetTrakException) e;
 		logger.error(e.getMessage(), exception);
-		
+
 		TargetTrakApiResponse apiResponse = exception.getResponse();
 		TargetTrakErrorTypeEnum errorType = apiResponse.getErrorType();
 
 		Map<String, Object> response = null;
 		switch (errorType) {
-			case VALIDATION:
-				response = handleValidationType(apiResponse);
-				break;
-			case WARNING:
-				response = handleWarningType(apiResponse);
-				break;
-			case ERROR:
-				response = handleErrorType(apiResponse);
-				break;
-			default:
-				response = handleDefaultType(apiResponse);
-				break;
+		case VALIDATION:
+			response = handleValidationType(apiResponse);
+			break;
+		case WARNING:
+			response = handleWarningType(apiResponse);
+			break;
+		case ERROR:
+			response = handleErrorType(apiResponse);
+			break;
+		default:
+			response = handleDefaultType(apiResponse);
+			break;
 		}
 		response.put(JSON_SUCCESS, apiResponse.isSuccess());
 		return response;
@@ -57,10 +58,26 @@ public class TargetTrakWebExceptionHandler {
 
 	private Map<String, Object> handleValidationType(TargetTrakApiResponse apiResponse) {
 		Map<String, Object> response = new HashMap<String, Object>();
-		response.put(JSON_MESSAGE, apiResponse.getMessage());
+		String apiErrorCode = getApiError(apiResponse.getErrors());
+
+		if (apiErrorCode == null) {
+			response.put(JSON_MESSAGE, apiResponse.getMessage());
+		} else {
+			response.put(JSON_MESSAGE, errorBuilder.buildErrorMessage(apiErrorCode));
+		}
+
 		List<UIValidationError> errors = errorBuilder.buildUiValidationErrors(apiResponse.getErrors());
 		response.put(JSON_ERRORS, errors);
 		return response;
+	}
+
+	private String getApiError(List<TargetTrakValidationError> errors) {
+		for (TargetTrakValidationError error : errors) {
+			if ("api".equals(error.getFieldName())) {
+				return error.getErrorMessage();
+			}
+		}
+		return null;
 	}
 
 	private Map<String, Object> handleWarningType(TargetTrakApiResponse apiResponse) {
