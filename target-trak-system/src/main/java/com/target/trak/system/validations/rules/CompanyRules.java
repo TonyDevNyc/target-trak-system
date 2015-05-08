d1525a808560e465c55af3782693d8d5e85ef324
TonyDevNyc <tonydevgroupny@gmail.com>
2015-05-08 15:03:12 -0400

    initial revision of company api business rules and address business rules

---
+++ target-trak-system/src/main/java/com/target/trak/system/validations/rules/CompanyRules.java
@@ -0,0 +1,13 @@
+package com.target.trak.system.validations.rules;
+
+import com.target.trak.system.validations.TargetTrakValidationError;
+
+public interface CompanyRules {
+
+	public TargetTrakValidationError isCompanyNameEmpty(final String companyName);
+
+	public TargetTrakValidationError isCompanyNameValidLength(final String companyName);
+
+	public TargetTrakValidationError companyNameContainsValidChars(final String companyName);
+
+}
