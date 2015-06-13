df3a4103d99c29d8a2d5e1ee49fc3b921c275503
TonyDevNyc <tonydevgroupny@gmail.com>
2015-06-12 20:49:45 -0400

    add timestamp utils

--- target-trak-system/src/main/java/com/target/trak/system/util/DateUtil.java
+++ target-trak-system/src/main/java/com/target/trak/system/util/DateUtil.java
@@ -1,5 +1,6 @@
 package com.target.trak.system.util;
 
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -19,5 +20,21 @@ public final class DateUtil {
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
 		String iso8601Date = df.format(new Date(calendar.getTimeInMillis()));
 		return iso8601Date;
+	}
+	
+	public static Calendar convertTimestampToCalendar(final Timestamp ts) {
+		if (ts == null) {
+			return null;
+		}
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(ts.getTime());
+		return c;
+	}
+	
+	public static Timestamp convertCalendarToTimestamp(final Calendar calendar) {
+		if (calendar == null) {
+			return null;
+		}
+		return new Timestamp(calendar.getTimeInMillis());
 	}
 }
